9de5116d475479f5b82151ffbdf7685db09d00de
EnterOrion <orionwesson@gmail.com>
2023-11-12 00:54:55 -0500

    Rough version without markdown

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,71 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	export let data;
+</script>
+
+<h1>Collected Fictions</h1>
+<main>
+	{#each data.posts as post (post.slug)}
+		<div class="post">
+			<a href={`/posts/${post.slug}`}>
+				<h2>
+					{post.title}
+				</h2>
+				<p class="post-synopsis">{post.synopsis}</p>
+				<p class="post-date">{post.date}</p>
+			</a>
+		</div>
+	{/each}
+</main>
+
+<style>
+	:global(body) {
+		background-color: rgb(246, 245, 241);
+	}
+
+	main {
+		padding-left: 2rem;
+	}
+
+	h1 {
+		font-size: 2.5rem;
+		text-decoration: underline;
+		margin-left: 1rem;
+		cursor: default;
+	}
+
+	h2 {
+		font-size: 1.5rem;
+	}
+
+	a {
+		text-decoration: none;
+		color: black;
+	}
+
+	.post a {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		border: 1px solid black;
+		padding-left: 1rem;
+		padding-right: 1rem;
+		max-width: 90%;
+		border-radius: 2px;
+	}
+
+	.post a:hover {
+		background-color: rgb(255, 242, 242);
+		border: 1px solid rgb(106, 106, 106);
+		cursor: pointer;
+	}
+
+	.post-synopsis {
+		font-style: italic;
+		margin-right: 25%;
+	}
+
+	.post-date {
+		font-weight: bold;
+		font-size: 1.2rem;
+	}
+</style>
---
+++ src/routes/posts/[slug]/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	export let data;
+</script>
+
+<h1>
+	{data.post.title}
+</h1>
+
+<p>
+	{data.post.content}
+</p>
