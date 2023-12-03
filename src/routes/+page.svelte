e6bb1fc0e8755dd05fac7c54274d241371e1be13
EnterOrion <orionwesson@gmail.com>
2023-12-03 17:25:31 -0500

    Final edits and add about page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,29 +6,46 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com" />
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
 	<link
-		href="https://fonts.googleapis.com/css2?family=Lato&family=Noto+Serif+Balinese&family=Nunito+Sans:opsz@6..12&family=Urbanist:wght@700&display=swap"
+		href="https://fonts.googleapis.com/css2?family=Lato&family=Noto+Serif+Balinese&family=Nunito+Sans:opsz@6..12&family=Roboto&family=Roboto+Mono:wght@200;300;400&family=Urbanist:wght@700&display=swap"
 		rel="stylesheet"
 	/>
 </svelte:head>
 
 <div class="container">
-	<h1>Collected Fictions</h1>
+	<div class="header-container">
+		<h1>Collected Fictions</h1>
+		<h2><a href="about">About</a></h2>
+	</div>
 
 	<div class="post-container">
 		{#each data.posts as post}
 			<div class="post">
 				<a href={`blog/${post.slug}`}>{post.title}</a>
-				<p class="post-date">{post.date}</p>
+				<p class="post-date">Published: {post.date}</p>
 			</div>
 		{/each}
 	</div>
 </div>
 
 <style>
+	.header-container {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+	}
 	h1 {
-		font-family: 'Urbanist', sans-serif;
-		font-size: 3rem;
+		font-family: 'Roboto Mono', monospace;
+		font-size: 2.5rem;
 		margin-left: 1rem;
+		margin-bottom: 1.2rem;
+		font-weight: 300;
+	}
+
+	h2 {
+		font-family: 'Roboto Mono', monospace;
+		font-size: 1.5rem;
+		margin-right: 4%;
+		font-weight: 300;
 		margin-bottom: 1.2rem;
 	}
 
@@ -37,18 +54,31 @@
 		cursor: default;
 	}
 
-	a,
+	h2:hover {
+		cursor: pointer;
+		text-decoration: underline;
+		color: grey;
+	}
+
+	h2 a {
+		color: black;
+		text-decoration: none;
+	}
+
+	.post a,
 	.post-date {
+		text-decoration: none;
 		font-size: 1.1rem;
 	}
 
-	a {
+	.post a {
 		font-family: 'Noto Serif Balinese', serif;
 		color: black;
 	}
 
 	a:hover {
 		color: grey;
+		text-decoration: underline;
 	}
 
 	.post {
@@ -67,6 +97,6 @@
 
 	.post-container {
 		margin-top: 1.2rem;
-		padding-left: 5%;
+		padding-left: 3%;
 	}
 </style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Lato&family=Noto+Serif+Balinese&family=Nunito+Sans:opsz@6..12&family=Roboto&family=Roboto+Mono:wght@200;300;400&family=Urbanist:wght@700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<h1>About</h1>
+<p>There's a thousand stories within me and I plan to write them all.</p>
+
+<style>
+	h1 {
+		font-family: 'Roboto Mono', monospace;
+		font-weight: 300;
+		font-size: 2.3rem;
+		margin-left: 1rem;
+		margin-bottom: 0.8rem;
+	}
+
+	p {
+		margin-top: 0.8rem;
+		font-family: 'Noto Serif Balinese', serif;
+		margin-left: 3%;
+	}
+</style>
